eca21774feaebb8a4a34a6080221d06162217236
Øyvind Bertelsen <roberto.oyvind@gmail.com>
2019-08-17 20:21:57 +0200

    Create 1-12 durotar.lua

---
+++ 1-12 durotar.lua
@@ -0,0 +1,70 @@
+Guidelime.registerGuide([[
+    [N1-12roadguide]
+    [D Offroad fast route test]
+    [NX12-20Offroad 12-20]
+    [GA Hunter,Troll]
+    Accept first quest[G67.83,32.85The Barrens][QA4641 Your place in the world]
+    Run to get [G66.43,29.75The Barrens][QA790]
+    Do [G66.38,31.96The Barrens][QC790] asap. Grind if not first, grp up with others.
+    Run back and turn in Sarkoth, grind on the way[G66.43,29.75The Barrens][QT790][G66.43,29.75The Barrens][QA804]
+    
+    [XP2-190 Reach 190exp remaining to accept Lazy Peons after turning in startquest + Sarkoth(farm on the way back)]
+    Turn in [G67.19,32.74The Barrens][QT804][G67.19,32.74The Barrens][QT4641] Accept [G67.19,32.74The Barrens][QA788][G67.19,32.74The Barrens][QA789][G67.19,32.74The Barrens][QA3082]
+    
+    
+    
+    [G67.54,32.17The Barrens][QA4402]
+    [V] Sell shit, buy some arrows
+    Turn in [G67.6,33.27The Barrens][QT3082]
+    Accept [G67.6,33.17The Barrens][QA792]
+    You should be level [XP3] now for Lazy Peon quest 
+    Accept [G68.53,32.91The Barrens][QA5441] and use item on first Peon if up.
+    Do all the following quests while following the western wall and go to Vile Familiars [G69.43,32.27,151The Barrens][QC5441][G68.27,29.39,162The Barrens][QC788][G66.41,29.67,110The Barrens][QC789][G68.84,30.49,118The Barrens][QC4402][G68.03,25.29,92The Barrens][QC792]
+    Apples respawn fast, stay outside cave for quick respawns. [O] go to [G41.95,56.61Durotar] for apples ppl don't take. 
+    
+    
+    Farm on the way back to turn in[G68.53,32.91The Barrens][QT5441] and accept [G68.53,32.91The Barrens][QA6394]
+    
+    Turn in [G67.6,33.17The Barrens][QT792] and accept [G67.6,33.17The Barrens][QA794]
+    
+    Run to vendor and [V] sell/buy arrows if needed \ (Remember 1s for serpent sting at level 4)
+    [G68.53,32.91The Barrens][QA6394]
+    [G67.53,24.72The Barrens][QC794]
+    [XP5+1055 Be around 1055 - 1100 exp into level, 3 bars away from halfway]
+    [G44.78,52.1Durotar][O die here]
+    [G72.35,19.79The Barrens][QA784]
+    [G71.28,18.17The Barrens][QA791]
+    [H Heartstone to startarea and turn in quests]
+    [G68.53,32.91The Barrens][QT6394]
+    [G67.6,33.17The Barrens][QT794]
+    [G67.19,32.74The Barrens][QT789]
+    [G67.54,32.17The Barrens][QT4402]
+    [V Sell stuff]
+    [G67.6,33.17The Barrens][QA805]
+    [T Train Arcane Shot & Hunter's Mark if you have over 4s 85c, skip hunter's mark if less]
+    [G72.41,32.73The Barrens][QA2161]
+    [G74.44,35.67The Barrens][QA817]
+    [G74.44,35.91The Barrens][QA818]
+    [G74.44,36.08The Barrens][QT805][G74.44,36.08The Barrens][QA808][G74.44,36.08The Barrens][QA823][G74.44,36.08The Barrens][QA826]
+    
+    
+    
+    [G56.46,73.11Durotar][V Buy new bow, 2s 85c]
+    [G73.55,35.33The Barrens][QA786]
+    [G69.74,37.87,104The Barrens][QC786]
+    [G46.43,79.25Durotar][O die to fire, ress at gy]
+    [R repair at vendor] [O buy axe 5s 40c] 
+    [G73.55,35.33The Barrens][QT786]
+    [O grind on the way to the humans]
+    [G75.04,24.83,145The Barrens][QC784,1 sailors]
+    [G76.22,26.8,153The Barrens][QC784,2 marines]
+    [G76.4,27.5The Barrens][QC784,3 benedict]
+    [G76.16,27.17The Barrens][QA830 upstairs]
+    
+    
+    
+    [OC]
+]], 'My Guides')
+-- See https://github.com/max-ri/Guidelime/wiki/WriteAGuide for instructions on how to write a guide.
+-- You can use *asterisks* for highlighting and \\ for linebreaks.
+-- The text 'My Guides' in the last line defines in which group your guide appears in the guides list. Use the same name for all of your guides.

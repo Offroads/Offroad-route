03c557152bc9c53adffe0df43ca996aaa8ef6081
Øyvind Bertelsen <roberto.oyvind@gmail.com>
2019-08-17 20:58:09 +0200

    Update 1-12 durotar.lua

--- 1-12 durotar.lua
+++ 1-12 durotar.lua
@@ -14,52 +14,58 @@ Guidelime.registerGuide([[
     
     
     [G67.54,32.17The Barrens][QA4402]
-    [V] Sell shit, buy some arrows
+    [V] Sell shit, *buy some arrows*
     Turn in [G67.6,33.27The Barrens][QT3082]
     Accept [G67.6,33.17The Barrens][QA792]
     You should be level [XP3] now for Lazy Peon quest 
     Accept [G68.53,32.91The Barrens][QA5441] and use item on first Peon if up.
     Do all the following quests while following the western wall and go to Vile Familiars [G69.43,32.27,151The Barrens][QC5441][G68.27,29.39,162The Barrens][QC788][G66.41,29.67,110The Barrens][QC789][G68.84,30.49,118The Barrens][QC4402][G68.03,25.29,92The Barrens][QC792]
-    Apples respawn fast, stay outside cave for quick respawns. [O] go to [G41.95,56.61Durotar] for apples ppl don't take. 
+    *Apples and mobs respawn fast, stay outside cave for quick respawns.* [O] *go to* [G41.95,56.61Durotar] *for apples ppl don't take.* 
     
     
     Farm on the way back to turn in[G68.53,32.91The Barrens][QT5441] and accept [G68.53,32.91The Barrens][QA6394]
     
     Turn in [G67.6,33.17The Barrens][QT792] and accept [G67.6,33.17The Barrens][QA794]
-    
     Run to vendor and [V] sell/buy arrows if needed \ (Remember 1s for serpent sting at level 4)
-    [G68.53,32.91The Barrens][QA6394]
-    [G67.53,24.72The Barrens][QC794]
-    [XP5+1055 Be around 1055 - 1100 exp into level, 3 bars away from halfway]
-    [G44.78,52.1Durotar][O die here]
-    [G72.35,19.79The Barrens][QA784]
-    [G71.28,18.17The Barrens][QA791]
-    [H Heartstone to startarea and turn in quests]
-    [G68.53,32.91The Barrens][QT6394]
-    [G67.6,33.17The Barrens][QT794]
-    [G67.19,32.74The Barrens][QT789]
-    [G67.54,32.17The Barrens][QT4402]
+    Run to cave to do [G68.06,25.16The Barrens][QC6394] and [G67.53,24.72The Barrens][QC794]
+    *Keep grinding until you are [XP5+1055] around 1055 - 1100 exp into level, 3 bars away from halfway*
+    *Run over to corner by the water [G44.78,52.1Durotar] [O] and die here.*
+    Accept the quests [G72.35,19.79The Barrens][QA784] and [G71.28,18.17The Barrens][QA791]
+    *[H] Heartstone to startarea and turn in quests*
+    Turn in all quests [G68.53,32.91The Barrens][QT6394][G67.6,33.17The Barrens][QT794][G67.19,32.74The Barrens][QT789][G67.54,32.17The Barrens][QT4402]
     [V Sell stuff]
-    [G67.6,33.17The Barrens][QA805]
-    [T Train Arcane Shot & Hunter's Mark if you have over 4s 85c, skip hunter's mark if less]
-    [G72.41,32.73The Barrens][QA2161]
-    [G74.44,35.67The Barrens][QA817]
-    [G74.44,35.91The Barrens][QA818]
-    [G74.44,36.08The Barrens][QT805][G74.44,36.08The Barrens][QA808][G74.44,36.08The Barrens][QA823][G74.44,36.08The Barrens][QA826]
+    You should have [G67.6,33.17The Barrens][QA805]
+    *[T Train Arcane Shot & Hunter's Mark if you have over 4s 85c, skip hunter's mark if less]*
+    Run and get [G72.41,32.73The Barrens][QA2161]
+    Accept [G74.44,35.67The Barrens][QA817]
+    Accept [G74.44,35.91The Barrens][QA818]
+    Turn in and accept [G74.44,36.08The Barrens][QT805][G74.44,36.08The Barrens][QA808][G74.44,36.08The Barrens][QA823][G74.44,36.08The Barrens][QA826]
     
     
     
-    [G56.46,73.11Durotar][V Buy new bow, 2s 85c]
-    [G73.55,35.33The Barrens][QA786]
-    [G69.74,37.87,104The Barrens][QC786]
-    [G46.43,79.25Durotar][O die to fire, ress at gy]
-    [R repair at vendor] [O buy axe 5s 40c] 
-    [G73.55,35.33The Barrens][QT786]
-    [O grind on the way to the humans]
-    [G75.04,24.83,145The Barrens][QC784,1 sailors]
-    [G76.22,26.8,153The Barrens][QC784,2 marines]
-    [G76.4,27.5The Barrens][QC784,3 benedict]
-    [G76.16,27.17The Barrens][QA830 upstairs]
+    *Run to vendor and buy new bow for 2s 85c*[V]
+    Go to patrolling npc to accept, usually standing still by the rocks[G73.55,35.33The Barrens][QA786]
+    Grind over to centaurs, careful with quick respawns[G69.74,37.87,104The Barrens][QC786]
+    Here you can die to the fire to respawn at Senjin[O]
+    [V]Buy axe 5s 40c and repair bow[R] 
+    Turn in [G73.55,35.33The Barrens][QT786]
+    [O] Kill every scorpid on the way, dont spend too much time. 
+    Kill mobs outside keep[G75.04,24.83,145The Barrens][QC784,1] Sailors
+    [G76.22,26.8,153The Barrens][QC784,2] Marines
+    [XP7 You should be level 7 now.]
+    Go up to kill[G76.4,27.5The Barrens][QC784,3] Benedict and loot key
+    Go upstairs to loot box and accept quest [G76.16,27.17The Barrens][QA830]
+    Keep grinding until [XP7 %50] so you can get 8 from turning in quests.
+    Die or run back to turn in [G72.51,19.61The Barrens][QT823] Accept [G72.51,19.61The Barrens][QA806]
+    Turn in [G72.13,18.82The Barrens][QT2161], set heartstone[S]
+    Accept [G71.91,19.24The Barrens][QA815]
+    [V] Vendor stuff
+    Go up the tower and turn in [G71.28,18.17The Barrens][QT791]
+    Go to the burrow and turn in [G72.35,19.79The Barrens][QA784] and [G72.35,19.79The Barrens][QT830] Accept the next part [G72.35,19.79The Barrens][QA831]
+    Accept [G72.35,19.79The Barrens][QA825] and [G72.35,19.79The Barrens][QA837]
+    You should be level [XP8] and ready to train. If not, go finish first part of Razormane
+    Go do the first part of [G71.1,22.56,92The Barrens][QC837,1] and [G70.65,22.61,124The Barrens][QC837,2]
+    
     
     
     

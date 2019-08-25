b4e58a0caf9bb5c5085fde18258245a1d43bcb15
Øyvind Bertelsen <roberto.oyvind@gmail.com>
2019-08-25 21:41:12 +0200

    Update 1-12 durotar.lua

--- 1-12 durotar.lua
+++ 1-12 durotar.lua
@@ -13,7 +13,7 @@ Turn in [G67.19,32.74The Barrens][QT804][G67.19,32.74The Barrens][QT4641] Accept
 
 
 
-[G67.54,32.17The Barrens][QA4402]
+[G67.54,32.17The Barrens][QA4402 Galgar's Cactus Apple Surprise]
 [V] Sell shit, *buy some arrows*
 Turn in [G67.6,33.27The Barrens][QT3082]
 Accept [G67.6,33.17The Barrens][QA792]
@@ -31,7 +31,7 @@ Run to cave to do [G68.06,25.16The Barrens][QC6394] and [G67.53,24.72The Barrens
 *Keep grinding until you are* [XP5+1055] *around 1055 - 1100 exp into level, 3 bars away from halfway*
 *Run over to corner by the water* [G44.78,52.1Durotar] [O] *and die here.*
 Accept the quests [G72.35,19.79The Barrens][QA784] and [G71.28,18.17The Barrens][QA791]
-[H] *Heartstone to startarea and turn in quests*
+[H]*Heartstone to startarea and turn in quests*[H]
 Turn in all quests [G68.53,32.91The Barrens][QT6394][G67.6,33.17The Barrens][QT794][G67.19,32.74The Barrens][QT789][G67.54,32.17The Barrens][QT4402]
 [V] Sell stuff
 You should have [G67.6,33.17The Barrens][QA805]
@@ -56,6 +56,8 @@ Kill mobs outside keep[G75.04,24.83,176The Barrens][QC784,1] Sailors
 Go up to kill[G76.4,27.5The Barrens][QC784,3] Benedict and *loot key*
 Go upstairs to loot box and accept quest [G76.16,27.17The Barrens][QA830]
 Keep grinding until [XP7 %50] so you can get 8 from turning in quests.
+
+
 *Die or run back* to turn in [G72.51,19.61The Barrens][QT823] Accept [G72.51,19.61The Barrens][QA806]
 Turn in [G72.13,18.82The Barrens][QT2161], set heartstone[S]
 Accept [G71.91,19.24The Barrens][QA815]
@@ -64,12 +66,108 @@ Go up the tower and turn in [G71.28,18.17The Barrens][QT791]
 Go to the burrow and turn in [G72.35,19.79The Barrens][QA784] and [G72.35,19.79The Barrens][QT830] Accept the next part [G72.35,19.79The Barrens][QA831]
 Accept [G72.35,19.79The Barrens][QA825] and [G72.35,19.79The Barrens][QA837]
 You should be level [XP8] and ready to train. If not, go finish first part of Razormane
-Go do the first part of [G71.1,22.56,125The Barrens][QC837,1] and [G70.65,22.61,177The Barrens][QC837,2]
+Go train [T] Prio *Concussive shot 2s* and *Raptor Strike 2s*. 
+[V]*Buy axe 5s 40c* [G52.02,40.63Durotar]
+[V]*Buy some arrows*[G52.99,41.08Durotar]
+[T]*Train First Aid 1s* 
+Go do the first part of [G71.1,22.56,125The Barrens][QC837,1] Razormane Quilboar and [G70.65,22.61,177The Barrens][QC837,2] Razormane Scout
+
+
+Loot box[G61.75,46.06Durotar][OC]
+Loot box[G62.01,46.36Durotar][OC]
+Loot Box[G62.44,46.02Durotar][OC]
+[QC825 From The Wreckage....]
+
+Start killing crabs for [QC818,1 A Solvent Spirit], [QC818,2 A Solvent Spirit]
+*DROWN IN THE OCEAN AND RESS IN SENJIN*
+
+Do echo isles stuff
+Finish [QC818 A Solvent Spirit]
+[QC815 Break a Few Eggs]
+[QC817 Practical Prey]
+[QC808 Minshina's Skull]
+[QC826 Zalazane]
+
+*Once complete, die and ress at Sen'jin*
+
+Turn in [G74.44,35.91The Barrens][QT818 A Solvent Spirit][G74.44,35.67The Barrens][QA817 Practical Prey][G74.44,36.08The Barrens][QT808 Minshina's Skull][QA826 Zalazane]
+
+
+Go to razor hill and turn in [G72.35,19.79The Barrens][QT825 From The Wreckage....] & [G71.91,19.24The Barrens][QT815 Break a Few Eggs]
+[R][R] Vendor and repair
+Do the rest of [G67.71,17.24,155The Barrens][QC837,3 Encroachment] [G67.5,17.47,173The Barrens][QC837 Encroachment]
+Accept [G67.74,12.87The Barrens][QA816 Lost But Not Forgotten]
+[G69.44,9.06The Barrens][QA834 Winds in the desert]
+Grind Harpys and loot satchels for [QC834 Winds in the Desert]
+Turn in[G69.44,9.06The Barrens][QT834 Winds in the Desert]
+Accept [G69.44,9.06The Barrens][QA835 Securing the Lines]
+Grind until [XP10-625] 625 exp left to 10. 
+Turn in[G72.35,19.79The Barrens][QT837 Encroachment]
+
+
+Accept [G72.3,19.79The Barrens][QA6062 Taming the beast]
+[T] Aspect of the Hawk 4s & Serpent Sting 4s & Parry 2s
+[V]Buy sharp arrows
+Tame Boar [QC6062 Taming the Beast]
+Turn in [G72.3,19.79The Barrens][QA6062 Taming the Beast]
+Accept [G72.3,19.79The Barrens][QA6083 Taming the Beast]
+Go tame the surf crawler [QC6083 Taming the Beast]
+Turn in[G72.3,19.79The Barrens][QT6083 Taming the Beast]
+Accept [G72.3,19.79The Barrens][QA6082 Taming the Beast]
+Tame Scorpid [QC6082 Taming the Beast]
+Turn in [G72.3,19.79The Barrens][QT6082 Taming the Beast]
+[G72.3,19.79The Barrens][QA6081 Training the Beast]
+
+[G71.78,19.84The Barrens][QA840 Conscript of the Horde]
+Go over here ish [G34.58,49.17Durotar] and *tame a Vanomtail Scorpid* kill crocolisk first, they can aggro.
+[G62.26,19.37The Barrens][QT840 Conscript of the Horde]
+*Die and ress at crossroads*
+Accept [G52.26,31.93The Barrens][QA870 The Forgotten Pools] and repair / vendor[V][R]
+Turn in [G52.23,31The Barrens][QT842 Crossroads Conscription]Accept [QA844 Plainstrider Menace]
+Accept [G52.62,29.84The Barrens][QA6365 Meats to Orgrimmar]
+Accept [G51.92,30.31The Barrens][QA869 Raptor Thieves]
+Accept [G51.49,30.87The Barrens][QA871 Disrupt the Attacks] and [QA5041 Supplies for the Crossroads]
+Accept [G51.44,30.15The Barrens][QA848 Fungal Spores] and [QA1492 Wharfmaster Dizzywig]
+Turn in [G51.5,30.33The Barrens][QT6365 Meats to Orgrimmar] and accept [QA6384 Ride to Orgrimmar]
+Fly to org[F]
+
+Go to thrall and accept [G31.78,37.81Orgrimmar][QA5726 Hidden Enemies] and turn in [G32.27,35.79Orgrimmar][QT831 The Admiral's Orders]
+Turn in [G26.22,95.85Azshara][QT6081 Training the Beast]
+*DELETE HS AND DIE WITH UNSTUCK*
+Go out and do [G72.45,8.46,179The Barrens][QC835 Securing the Lines]
+Turn it in [G69.44,9.06The Barrens][QT835 Securing the Lines] and get new bow
+Go to the river and kill crocs [G63.46,11.15,150The Barrens][QC816 Lost But Not Forgotten]
+Go kill Fizzle[G67.22,11.01The Barrens][QC806 Dark Storms]
+Grind on the way to turn in [G67.74,12.87The Barrens][QT816 Lost But Not Forgotten]
+Go accept [G72.51,19.61The Barrens][QA828 Margoz]
+*GET NEW HS FROM INN* and vendor / repair [R][V]
+Run up and turn in [G74.68,7.55The Barrens][QT828 Margoz] and accept [QA827 Skull Rock]
+Run into cave and do [G72.86,1.95,149The Barrens][QC827 Skull Rock]
+Kill Gazz'uz  /target to find him, can be left or right spawn. Use skull from Echo isles quest to kill him. Loot and accept [QA832 Burning Shadows]
+[QC5726 Hidden Enemies] should also be done before leaving.
+Go out and turn in [G74.68,7.55The Barrens][QT827 Skull Rock] accept [QA829 Neeru Fireblade]
+Go to org and turn in [G46.84,4.94Durotar][QT6384 Ride to Orgrimmar] accept [QA6385 Doras the Wind Rider Master]
+Go up to flight master and turn it in [G44.46,3.74Durotar][QT6385 Ride to Orgrimmar] accept [QA6386 Return to the Crossroads.]
+Go to Thrall and turn in [G31.78,37.81Orgrimmar][QA5726 Hidden Enemies] accept [QA5727 Hidden Enemies]
+Go to Cleft of shadows and turn in [G45.61,0.21Durotar][QT829 Neeru Fireblade] accept [QA809 Ak'Zeloth] and turn in [QT832 Burning Shadows]
+Complete [QC5727 Hidden Enemies] by talking to npc. Click text hold shift
+Go back to Thrall and turn in [G31.78,37.81Orgrimmar][QT5727 Hidden Enemies]
+[H]*Heartstone to Razor Hill*[H]
+
+Run over bridge to barrens and turn in[G62.33,20.07The Barrens][QT809 Ak'Zeloth] accept [QA924 The Demon Seed]
+*Loot power stone*
+Start farming striders and Raptors - go get Keg[G55.8,20The Barrens][QA819 Chen's Empty Keg]
+Go tame a level 12 cat[G51.4,21.88The Barrens]
+Go up the mountain and do [G47.97,19.07The Barrens][QC924 The Demon Seed]
+Go do [G55.46,26.65,124The Barrens][QC871 Disrupt the Attacks] and check for keg if still missing it [QA819 Chen's Empty Keg]
+Go turn in [G44.46,3.74Durotar][QA6386 Return to the Crossroads.]
+Turn in [G52.23,31The Barrens][QT844 Plainstrider Menace] accept [QA845 The Zhevra]
+[H]*Set HS to crossroads*[H]
 
 
 
 
-[OC]
+
 ]], 'My Guides')
 -- See https://github.com/max-ri/Guidelime/wiki/WriteAGuide for instructions on how to write a guide.
 -- You can use *asterisks* for highlighting and \\ for linebreaks.
